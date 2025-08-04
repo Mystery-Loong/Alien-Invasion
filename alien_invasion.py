b7e3a32baea646e61f3866ee88b9dfa715b6d7b2
Mystery-Loong <15277112806@139.com>
2025-08-04 19:16:27 +0800

    ubuntu update data 8.4

--- alien_invasion.py
+++ alien_invasion.py
@@ -37,13 +37,19 @@ class AlienInvasion:
 
         self._create_fleet()
 
+        # 游戏启动后处于活动状态
+        self.game_ative = True
+
     def run_game(self):
         """开始游戏的主循环"""
         while True:
             self._check_events()
-            self.ship.update()
-            self._update_bullets()
-            self._update_aliens()
+
+            if self.game_ative:
+                self.ship.update()
+                self._update_bullets()
+                self._update_aliens()
+                
             self._update_screen()
             self.clock.tick(60)
 
@@ -112,7 +118,10 @@ class AlienInvasion:
 
         # 检测外星人和飞船之间的碰撞
         if pygame.sprite.spritecollideany(self.ship,self.aliens):
-            print("Ship hit!!!")
+            self._ship_hit()
+
+        # 检查是否有外星人到达了屏幕的下边缘
+        self._check_aliens_bottom()
 
     def _create_fleet(self):
         """创建一个外星舰队"""
@@ -162,6 +171,33 @@ class AlienInvasion:
 
         # 让最近绘制的屏幕可见
         pygame.display.flip()
+
+    def _ship_hit(self):
+        """响应飞船和外星人的碰撞"""
+        if self.stats.ships_left > 0:
+            # 将 ship_left 减1
+            self.stats.ships_left -= 1
+
+            # 清空外星人列表和子弹列表
+            self.bullets.empty()
+            self.aliens.empty()
+
+            # 创建一个新的外星人舰队，并将飞船放在屏幕底部的中央
+            self._create_fleet()
+            self.ship.center_ship()
+
+            # 暂停 
+            sleep(0.5)
+        else:
+            self.game_ative = False
+
+    def _check_aliens_bottom(self):
+        """检查是否有外星人到达了屏幕的下边缘"""
+        for alien in self.aliens.sprites():
+            if alien.rect.bottom >= self.settings.screen_height:
+                # 像飞船被撞到一样进行处理
+                self._ship_hit()
+                break
         
 if __name__ == '__main__':
     # 创建游戏实例并运行游戏
